1bebef01b9e67fdc0d6f5eb643773135d6dc918b
Philipid3s <julien.regnault@bradyplc.com>
2018-10-10 14:16:39 +0800

    config file

--- app/api/index.js
+++ app/api/index.js
@@ -2,6 +2,8 @@ const config = require('config');
 const Mastodon = require('mastodon-api');
 
 const configmastodon = {
+        client_key: config.mastodon.clientkey,
+        client_secret: config.mastodon.clientsecret,
         access_token: config.mastodon.accesstoken,
         api_url: 'https://mastodon.social/api/v1/'
 }
